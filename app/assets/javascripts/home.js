5edc2690e366a5f4b1179e324dd0395c45e9dcc9
Brian Winterling <bwinterling@yahoo.com>
2014-02-24 09:48:43 -0700

    Tweaks to formating, added ibotta header and background

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,7 +13,7 @@ $(function(){
   var listOffers = function(data){
     var ul = $("<ul class='offer-list'>");
     for (var i = 0, l = data.length; i < l; ++i) {
-      ul.append("<li class='offer'>" + 
+      ul.append("<li class='offer col-md-4'>" + 
         "<img class='offer-image' src='" + data[i]['image_url'] + "' alt='" + data[i]['name'] + "'>" + 
         "<p class='offer-desc'>" + data[i]['description'] + "</p>" + 
         appendTasks(data[i]['tasks']) + "</li>");
