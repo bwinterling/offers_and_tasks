457fc460f22b36b30c1830cec0c39fb8ead251c0
Brian Winterling <bwinterling@yahoo.com>
2014-02-24 10:25:43 -0700

    minor tweak to jquery handling of ul element

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,22 +4,20 @@ $(function(){
       // add the tasks for each offer
       var taskList = "<ul class='task-list'>";
       for (var i = 0, l = tasks.length; i < l; ++i) {
-        taskList = taskList + 
-        "<li class='task " + tasks[i]['task_type'] + "'></li>";
-      }
+        taskList += "<li class='task " + tasks[i]['task_type'] + "'></li>";
+      };
+      taskList += "</ul>"
       return taskList
   }
 
   var listOffers = function(data){
-    var ul = $("<ul class='offer-list'>");
+    var offerList = $('ul.offer-list');
     for (var i = 0, l = data.length; i < l; ++i) {
-      ul.append("<li class='offer col-md-4'>" + 
+      offerList.append("<li class='offer col-md-4'>" + 
         "<img class='offer-image' src='" + data[i]['image_url'] + "' alt='" + data[i]['name'] + "'>" + 
         "<p class='offer-desc'>" + data[i]['description'] + "</p>" + 
         appendTasks(data[i]['tasks']) + "</li>");
     }
-
-    $("#offers").append(ul);    // add the list to the DOM
   };
 
   var fetchOffers = function(callback){
